2d128e3e3df23ca81ac16eff694c29bac833f04d
Joe Wegner <jwegner@printarts.com>
2011-03-19 08:26:13 -0500

    Added useful logout page

--- logout.php
+++ logout.php
@@ -5,5 +5,7 @@ unset($_SESSION['user_id']);
 unset($_SESSION['gid']);
 unset($_SESSION['is_admin']);
 
+
+header("Location: index.php");
 //blank page right now.. just using this to logout for testing.
 ?>
